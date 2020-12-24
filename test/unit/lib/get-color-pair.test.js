1cba22384b2c5dfa87cc7375e663fa454967acb2
chee <chee@snoot.club>
2020-12-24 18:26:19 +0000

    extract and test color pairs

---
+++ test/unit/lib/get-color-pair.test.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const assert = require('proclaim');
+const getColorPair = require('../../../lib/get-color-pair');
+const contrast = require('color-contrast');
+
+describe('lib/get-color-pair', () => {
+	describe('.pairs', () => {
+		for (const [foreground, background] of getColorPair.pairs) {
+			describe(`${foreground} on ${background}`, () => {
+				it('is a high enough contrast', () => {
+					// We're doing a bold, quite large font.
+					// Aiming for a bit above AA.
+					const HIGH_ENOUGH_CONTRAST = 5;
+					assert.greaterThan(
+						contrast(foreground, background),
+						HIGH_ENOUGH_CONTRAST
+					);
+				});
+			});
+		}
+	});
+	describe('getColorPair()', () => {
+		it('returns one of the pairs', () => {
+			assert(getColorPair.pairs.includes(getColorPair()));
+		});
+	});
+});
