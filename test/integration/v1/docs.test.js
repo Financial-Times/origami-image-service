91315bd914ce3ab79c4ed3af843e7e6e3c53f374
Jake Champion <me@jakechampion.name>
2022-04-12 17:50:20 +0100

    Remove the old routes which were not under /__origami/service/image
    These routes are no longer used anymore as we changed our CDN
    configuration to no longer remove the /__origami/service/image path
    from the request before sending it to our servers.

--- test/integration/v1/docs.test.js
+++ test/integration/v1/docs.test.js
@@ -4,16 +4,10 @@ const itRespondsWithHeader = require('../helpers/it-responds-with-header');
 const itRespondsWithStatus = require('../helpers/it-responds-with-status');
 const setupRequest = require('../helpers/setup-request');
 
-describe('GET /v1/', function() {
-	setupRequest('GET', '/v1/');
+describe('GET /', function() {
+	setupRequest('GET', '/');
 	itRespondsWithStatus(301);
 	itRespondsWithHeader('Location', '/__origami/service/image/v2/');
-});
-
-describe('GET /v1/images/raw/fticon:cross?source=origami-image-service', function() {
-	setupRequest('GET', '/v1/images/raw/fticon:cross?source=origami-image-service');
-	itRespondsWithStatus(301);
-	itRespondsWithHeader('Location', '/__origami/service/image/v2/images/raw/fticon:cross?source=origami-image-service');
 });
 
 describe('GET /__origami/service/image/v1/', function() {
