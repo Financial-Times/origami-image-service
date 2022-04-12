4193731247110f6d9e6b4812dff0550217b9baa5
Jake Champion <me@jakechampion.name>
2022-04-12 19:48:12 +0100

    convert tests from supertest to axios (#796)
    This changes the way the tests are written so
    that a single request is used to satisfy multiple
    assertion rather than a single request per assertion

---
+++ test/integration/helpers/axios.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const process = require('process');
+const axios = require('axios').default;
+
+module.exports = axios.create({
+    baseURL: process.env.HOST || 'http://localhost:8080',
+    maxRedirects: 0,
+	decompress: false,
+    validateStatus: function (status) {
+        return status >= 200 && status < 599;
+    },
+});
