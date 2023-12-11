f817b5bdd4cf25af63abeac034ca81c374319cd6
Akaki Mikaia <akakimikaia@gmail.com>
2023-12-11 18:01:31 +0000

    backstage: add simple logic of queuing jobs

--- scripts/worker.js
+++ scripts/worker.js
@@ -1,7 +1,36 @@
 'use strict';
+const throng = require('throng');
+const Queue = require('bull');
 
-function main() {
-  console.log('Hello from worker.js');
+// Connect to a Heroku-provided URL of Redis instance
+const REDIS_URL = process.env.REDIS_URL;
+
+// Spin up multiple processes to handle jobs to take advantage of more CPU cores
+// See: https://devcenter.heroku.com/articles/node-concurrency for more info
+const workers = process.env.WEB_CONCURRENCY || 2;
+
+// The maximum number of jobs each worker should process at once. This will need
+// to be tuned for your application. If each job is mostly waiting on network
+// responses it can be much higher. If each job is CPU-intensive, it might need
+// to be much lower.
+const maxJobsPerWorker = 50;
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-main();
+function start() {
+  // Connect to the named work queue
+  const workQueue = new Queue('hostnames', REDIS_URL);
+
+  workQueue.process(maxJobsPerWorker, async (job) => {
+    console.log({ job });
+    // A job can return values that will be stored in Redis as JSON
+    // This return value is unused in this demo application.
+    return { value: 'This will be stored' };
+  });
+}
+
+// Initialize the clustered worker process
+// See: https://devcenter.heroku.com/articles/node-concurrency for more info
+throng({ workers, start });
